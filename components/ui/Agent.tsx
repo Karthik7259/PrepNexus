aeee1abbc0428476f1eab2b5a239b3c7465e00d5
Karthik7259 <mohankartik.cs23@rvce.edu.in>
2025-05-24 00:41:08 +0530

    interview pages

---
+++ components/ui/Agent.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import Image from 'next/image'
+import { buttonVariants } from './button';
+import { cn } from '@/lib/utils'; // or wherever your utility is defined
+
+enum CallStatus{
+  INACTIVE = 'INACTIVE',
+  CONNECTING = 'CONNECTING',
+  ACTIVE = 'ACTIVE',
+  FINISHED = 'FINISHED',
+}
+
+
+const Agent = ({ userName } :  AgentProps) => {
+  const callstatus=CallStatus.ACTIVE;
+  const isSpeaking = true;
+  const messages=[
+    'Whats your name ?',
+    'My name is John Doe,nice to meet you!'
+  ];
+  const lastMessage=messages[messages.length-1];
+
+
+  return (
+    <>
+    <div className='call-view'>
+       <div className='card-interviewer'>
+               <div className='avatar'>
+                <Image src="/ai-avatar.png" alt="ai-avatar" width={65} height={54} className='object-cover' />
+                {isSpeaking && <span className='animate-speak'/>}
+              
+                </div>
+                
+                <h3>Ai Interviwer</h3>
+
+       </div>
+
+    <div className='card-border'> 
+         <div className='card-content'>
+            <Image src="/user-avatar.png" alt="ai-avatar" width={540} height={540} className='rounded-full object-cover size-[120px]' />
+            <h3>
+              {userName}
+            </h3>
+         </div>
+    </div>
+    </div>
+    {messages.length > 0 && (
+      <div className='transcript-border'>
+        <div className='transcript'>
+          <p key={lastMessage} className={cn('transition-opacity duration-500 opacity-0','animate-fadeIn opacity-100')}>
+             {lastMessage}
+          </p>
+        </div>
+      </div>
+    )}
+
+    
+      <div className='w-full flex  justify-center'>
+     {callstatus != 'ACTIVE' ? (
+        <button className="relative btn-call">
+          <span className={cn('absolute animate-ping rounded-full opacity-75', callstatus != 'CONNECTING' && 'hidden')}
+          />
+            <span>
+            {callstatus === 'INACTIVE' || 
+            callstatus==='FINISHED' ? 'Call' : '. . .'}
+
+            </span>
+         
+        </button>
+     ) : (
+       <button className='btn-disconnect'>
+        End Call
+       </button>
+     )}
+      </div>
+    </>
+    
+  )
+}
+
+export default Agent
